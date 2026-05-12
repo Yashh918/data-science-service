697fb3d48f91eca8a3747bed0c23e038e88417e0
Yashh918 <yashdhopeshwarkar@gmail.com>
2026-05-12 13:21:32 +0530

    feat: integrate Kafka producer for message handling

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask import request, jsonify
+from kafka import KafkaProducer
+import json
 from service.messageService import MessageService
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 messageService = MessageService()
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+    api_version=(2, 5, 0)
+)
 
 @app.route('/api/v1/ds/message', methods=['POST'])
 def handle_message():
@@ -15,7 +22,12 @@ def handle_message():
     if result is None:
         return jsonify({"error": "Not a Bank SMS"}), 400
 
-    return jsonify(result.dict()), 200
+    
+    result_dict = result.model_dump()
+    
+    producer.send('bank.message', result_dict)
+
+    return jsonify(result_dict), 200
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8010, debug=True)
